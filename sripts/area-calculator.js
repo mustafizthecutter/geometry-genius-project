79d9ce013840dd9f7f6e2c504595fa98745dea08
Mustafizur Rahman <mustafizur232@gmail.com>
2023-10-05 14:18:23 +0600

    rectangle area done

--- sripts/area-calculator.js
+++ sripts/area-calculator.js
@@ -6,5 +6,22 @@ function getTriangleValueById() {
     const heightValue = document.getElementById('triangle-height');
     const heightValueString = heightValue.value;
     const height = parseFloat(heightValueString);
-    console.log(height);
+    const area = 0.5 * base * height;
+
+    const triangleArea = document.getElementById('triangle-area');
+    triangleArea.innerText = area;
+}
+
+function getRectangleValueById() {
+    const widthValue = document.getElementById('rectangle-width');
+    const widthValueString = widthValue.value;
+    const width = parseFloat(widthValueString);
+
+    const lengthValue = document.getElementById('rectangle-length');
+    const lengthValueString = lengthValue.value;
+    const length = parseFloat(lengthValueString);
+    const area = width * length;
+
+    const rectangleArea = document.getElementById('rectangle-area');
+    rectangleArea.innerText = area;
 }
